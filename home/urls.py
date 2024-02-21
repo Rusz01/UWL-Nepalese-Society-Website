b2dd8af70f950fdd55cd7f47eab3f17e96482e7b
Rusz01 <ruszbaidhya@gmail.com>
2024-02-21 21:10:22 +0000

    Adding urls

---
+++ home/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from home import views
+
+urlpatterns = [
+#    URLS for Pages
+    path('', views.home, name='home'),
+    path('contact', views.contact, name='contact'),
+    path('event', views.event, name='event'),
+    path('members', views.members, name='members'),
+]
