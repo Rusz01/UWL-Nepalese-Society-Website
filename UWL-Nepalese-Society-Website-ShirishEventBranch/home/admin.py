9f61ab2fe153c4a26d7b82701b743353c5376027
Shirish59 <shresthashirish38@gmail.com>
2024-03-21 19:33:28 +0000

    Deleting fault file

--- UWL-Nepalese-Society-Website-ShirishEventBranch/home/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from home.models import Event
-from home.models import RecentEvent
-# Register your models here.
-admin.site.register(Event)
-admin.site.register(RecentEvent)
