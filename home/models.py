82c36f14e570886f25d1c579f5001553e1e6203d
Shirish59 <shresthashirish38@gmail.com>
2024-03-22 12:44:13 +0000

    adding the tinymce

--- home/models.py
+++ home/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from tinymce.models import HTMLField
 # Create your models here.
 class Event(models.Model):
     id = models.AutoField(primary_key=True)
     image=models.ImageField(upload_to="image")
     heading = models.CharField(max_length=100,  blank=True, null=True)
-    caption=models.CharField(max_length=1000)
+    caption=HTMLField()
     booking_link = models.URLField(blank=True, null=True)
     EventCreatedDate = models.DateTimeField(auto_now_add=True, null = True)
     def __str__(self):
@@ -15,7 +15,7 @@ class RecentEvent(models.Model):
     id = models.AutoField(primary_key=True)
     image=models.ImageField(upload_to="recentimage")
     heading = models.CharField(max_length=100,  blank=True, null=True)
-    caption=models.CharField(max_length=1000)
+    caption=HTMLField()
     RecentEventCreatedDate = models.DateTimeField(auto_now_add=True, null = True)
     def __str__(self):
         return self.heading
